0fffe2bb111c834e45e61083283df742353b8f87
Liqimai <liqimai@outlook.com>
2016-06-02 20:06:54 +0800

    Add struct_or_union_specifier: struct_or_union IDENTIFIER to BNF

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from yyparse.ZCCparser import parser, printAST
 from yyparse.ZCClex import lexer as ZCClexer
 
 if __name__ == '__main__':
-    with open("symbol/test.c") as f:
+    # with open("symbol/test.c") as f:
+    with open("symbol/preprocessed.c") as f:
+
         codes = f.read()
         pt = parser.parse(codes, lexer=ZCClexer)
         print "errorCounter=", parser.errorCounter
-        print pt
         printAST(pt)
--- public/global.py
+++ public/global.py
@@ -1,11 +1,31 @@
 #common structure put here
 from const import *
 
-class TreeNode:
-    child    = []
-    sibling  = None
-    lineno   = UNDEFINED  # line number
-    nodeKind = UNDEFINED  # enum in NodeKind
-    kind     = UNDEFINED  # enum in StmtKind, ExpKind
-    attr     = UNDEFINED  # enum in ExpKind
-    type_     = UNDEFINED  # enum in ExpType
+class TreeNode(object):
+    kind = str()  #
+    children = list()  # TreeNode
+    attr = Attr()
+
+
+class Attr(object):
+    # IDENTIFIER
+    value = str()  # i,j
+    type = Ctype()
+
+    is_global=bool()
+    is_static=bool()
+    addVariableTable=list() # compound_statment
+
+class Struct(object):
+    member = {"IDENTIFIER":(ctype, offset = int())}
+
+class Ctype(object):
+    def __init__(self):
+        self.type = str()
+        # "int","char","double","float","long","short","void",
+        # "struct","union","enum","function"
+        self.is_signed = bool()
+        self.is_const = [False]  #
+        self.pointer_count = lambda: (len(self.is_const) - 1)
+        self.struct = Struct()
+        self.size = int()
