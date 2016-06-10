c50b5a53bb9f1d10445d157d580fb1d9eeb2d6ab
ChoPin <1159597103@qq.com>
2016-06-10 17:59:40 +0800

    update readme about optimization

--- main.py
+++ main.py
@@ -3,18 +3,21 @@ from yyparse.ZCCparser import parser, printAST
 from yyparse.ZCClex import lexer as ZCClexer
 from symbol.symtab import c_types
 from public.ZCCglobal import global_context,FuncType
+from generation.generation import generator
 
 if __name__ == '__main__':
     # with open("yyparse/missSEMI.c") as f:
     # with open("yyparse/missRightCurly.c") as f:
     # with open("symbol/test.c") as f:
     # with open("symbol/preprocessed.c") as f:
-    with open("test/a.c") as f:
-
+    with open("test/test1.c") as f:
         codes = f.read()
         pt = parser.parse(codes, lexer=ZCClexer)
         print "errorCounter=", parser.errorCounter
-        printAST(pt)
-    print(global_context)
-
-
+        #printAST(pt)
+        #with open("test.s","w") as output:
+    #printAST(global_context.local['main'].compound_statement.ast)
+    gen=generator()
+    gen.generate()
+    gen.output('test/out.txt')
+    
