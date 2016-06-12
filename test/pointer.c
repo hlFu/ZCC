e6de83f7f6e99a619b0db012ad74a172cbccf6dc
flyingkite <982727566@qq.com>
2016-06-12 22:37:06 +0800

    add examples

---
+++ test/pointer.c
@@ -0,0 +1,21 @@
+/*
+ * embedded functional pointer
+ */
+#include "stdio.h"
+int i;
+
+void print_int(){
+    printf("%d\n", i);
+}
+
+void (*high_order_func(int n)) (){
+    i = n;
+    return print_int;
+}
+
+int main(){
+    void (*(*f)(int n))();
+    f = high_order_func;
+    f(2)();
+    return 0;
+}
